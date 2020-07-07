62dd57e3a76f37bb03754c6f96c730d4095d7e30
bumbazimba <58372370+bumbazimba@users.noreply.github.com>
2020-07-07 11:41:55 +0200

    Add files via upload

--- script.ps1
+++ script.ps1
@@ -1,88 +1,172 @@
-﻿#This script builds several branches for an Application in AppCenter and saves a report.
+#This script builds several branches for an Application in AppCenter and saves a report.
 #Check Readme.md file for more details - https://github.com/bumbazimba/appcenter_automatization/blob/master/README.md
 #Designed for Powershell 6 or above. Tested on Powershell 7
 #Author Nikita Nikolaev
-#Version 1.0
+#Version 1.1
 
 #Enter AppCenter credentials for API requests
-	$user = Read-Host "Enter AppCenter username"
-	$app = Read-Host "Enter Application name"
-	$token = Read-Host "Enter API Token"
-	[int]$branches_number = Read-Host "Enter number of branches to build"
-    $branches_number = $branches_number-1
 
-#Create Excel file and insert table headers
-	$excel = New-Object -ComObject excel.application
-	$excel.visible = $True
-	$workbook = $excel.Workbooks.Add()
-	$sheet= $workbook.Worksheets.Item(1)
-	$sheet.Name = 'Report'
-	$sheet.Cells.Item(1,1)= 'Branch name'
-	$sheet.Cells.Item(1,2)= 'Build status'
-	$sheet.Cells.Item(1,3)= 'Duration (min)'
-	$sheet.Cells.Item(1,4)= 'Link to build logs'
-	$row = 2
+[string]$user = Read-Host "Enter AppCenter username"
+[string]$app = Read-Host "Enter Application name"
+[string]$token = Read-Host "Enter API Token"
+[int]$branches_number = Read-Host "Enter number of branches to build"
+$branches_number = $branches_number-1
 
-#Get All branches from the Application and save branches names in array
+
+$excel = Create-ExcelFile
+$branches = Get-Branches
+$build = Build-Branches $branches
+$build_details = Get-BuildDetails $build
+Insert-ExcelFile $excel, $build_details, $branches
+Save-ExcelFile $excel
+
+
+
+#Get All branches from the Application
+function Get-Branches ()
+{
 	$branches = Invoke-RestMethod -Uri "https://api.appcenter.ms/v0.1/apps/$user/$app/branches" -Method Get `
 		-Headers @{"Accept"="application/json"; "X-API-Token"="$($token)"; "Content-Type"="application/json"} -MaximumRetryCount 5 -RetryIntervalSec 2;
 	Write-Host "`nList of all branches in Application $app :"
-	$branches.branch.name
-	$branches = $branches.branch.name
+    Write-Host $branches
+    return $branches
+}
 
-#Build First $branches_number branches and save IDs for next step
-	$build_id = @()
 
-	Foreach ($branch in $branches[0..$branches_number])	
+#Build specified number of branches
+function Build-Branches ()
+{
+    param
+    (
+        [Parameter(Mandatory=$true)][object]$branches
+    )
+
+	$build = @()
+    
+	#Check if user wants to build All or just few branches
+	[int]$branches_count = $branches.Length - 1
+    $branches_names = $branches.branch.name
+        if ($branches_count -ne $branches_number)
+        {
+            $branches_count = $branches_number
+        }
+        else 
+        {
+            $branches_count = $branches_count - 1
+            $branches_number = $branches_count
+        }
+	Foreach ($branch in $branches_names[0..$branches_count])	
 		{ 		
-			$build_id += Invoke-RestMethod -Uri "https://api.appcenter.ms/v0.1/apps/$user/$app/branches/$branch/builds" -Method Post `
-				-Headers @{"Accept"="application/json"; "X-API-Token"="$($token)"; "Content-Type"="application/json"} -MaximumRetryCount 5 -RetryIntervalSec 2 | Select-object id
-			Write-host "Branch $branch - build started"
-			$sheet.Cells.Item($row,1)=$branch
-			$row++
+			$build += Invoke-RestMethod -Uri "https://api.appcenter.ms/v0.1/apps/$user/$app/branches/$branch/builds" -Method Post `
+				-Headers @{"Accept"="application/json"; "X-API-Token"="$($token)"; "Content-Type"="application/json"} `
+				-MaximumRetryCount 5 -RetryIntervalSec 2 | Select-object id
+			Write-Host "Branch $branch - build started"
 		}
-#Get Duration, Status and Log file link for every build by ID
-	$row=2
+        return $build
+}
+
+#Get Details about all Builds
+function Get-BuildDetails ()
+{
+        param
+    (
+        [Parameter(Mandatory=$true)][object]$build
+    )
+	
 	$result = "in progress"
 	$number = 0
-	Write-host "`n"
-		Foreach ($id in $build_id)
+    $build_result = @()
+		Foreach ($id in $build)
 		{
-			$id = $build_id[$number].id
-			#.API request of build's details. If build still in progress, wait 30 seconds
+			$id = $build[$number].id
+			#API request of build's details. If build is still in progress, wait 20 seconds
 				while (($result -ne "failed") -and ($result -ne "success"))
 				{
 					$details = Invoke-RestMethod -Uri "https://api.appcenter.ms/v0.1/apps/$user/$app/builds/$id" -Method Get `
-						-Headers @{"Accept"="application/json"; "X-API-Token"="$($token)"; "Content-Type"="application/json"} -MaximumRetryCount 5 -RetryIntervalSec 2;
+						-Headers @{"Accept"="application/json"; "X-API-Token"="$($token)"; "Content-Type"="application/json"} `
+						-MaximumRetryCount 5 -RetryIntervalSec 2;
 					$result = $details.result
                     Write-Host "Build ID $id is in progress"
-					Start-Sleep -s 30
+					Start-Sleep -s 20
 				}
-				#Calculating Duration time
-					$starttime = $details.startTime
-					$finishtime = $details.finishTime
-					$diff= New-TimeSpan -Start $starttime -End $finishtime
-					$duration = $diff.ToString()
-				#Generate log download link	
-					$log = "https://appcenter.ms/download?url=%2Fv0.1%2Fapps%2F"+$user+"%2F"+$app+"%2Fbuilds%2F"+$id+"%2Fdownloads%2Flogs"
-				#Insert data to Excel table
-					$sheet.Cells.Item($row,2)=$result
-					$sheet.Cells.Item($row,3)=$duration
-					$sheet.Cells.Item($row,4)=$log
-					$row++
-					$number++
-					$result = "in progress"
-					Write-Host "Build ID $id completed!"
+                $build_result += $details
+				$number++
+				$result = "in progress"
+				Write-Host "Build ID $id completed!"            
 		}
+        return $build_result
+}
+
+
+#Create Excel file and insert table headers
+function Create-ExcelFile ()
+{
+
+	$excel = New-Object -ComObject excel.application
+	$excel.visible = $True
+    $workbook = $excel.Workbooks.Add()
+	$sheet= $workbook.Worksheets.Item(1)
+	$sheet.Name = 'Report'
+	$excel.Cells.Item(1,1)= 'Branch name'
+	$excel.Cells.Item(1,2)= 'Build status'
+	$excel.Cells.Item(1,3)= 'Duration (min)'
+	$excel.Cells.Item(1,4)= 'Link to build logs'
+    return $excel
+}
+
+#Insert build details in Excel file
+function Insert-ExcelFile ()
+{  
+        param
+    (
+        [Parameter(Mandatory=$true)][object]$excel,
+        [int]$row = 2
+    )  
+
+    #Get and Insert Branch names in Excel file
+        $name = $branches.branch.name     
+        Foreach ($id in $name[0..$branches_number])
+        {
+            $excel.ActiveWorkbook.ActiveSheet.Cells.Item($row,1) = $id
+            $row++
+        }
+
+    #Get and Insert other build Details in Excel file
+        $row = 2
+        Foreach ($build in $build_details) 
+        {    
+            $result=$build.result
+        #Calculate Duration time
+	        $starttime = $build.startTime
+	        $finishtime = $build.finishTime
+	        $diff= New-TimeSpan -Start $starttime -End $finishtime
+	        $duration = $diff.ToString()
+
+	    #Generate log download link	
+            $build_id = $build.id
+	        $log = "https://appcenter.ms/download?url=%2Fv0.1%2Fapps%2F"+$user+"%2F"+$app+"%2Fbuilds%2F"+$build_id+"%2Fdownloads%2Flogs"
+
+	    #Insert data to the Excel table
+	        $excel.ActiveWorkbook.ActiveSheet.Cells.Item($row,2)=$result
+	        $excel.ActiveWorkbook.ActiveSheet.Cells.Item($row,3)=$duration
+	        $excel.ActiveWorkbook.ActiveSheet.Cells.Item($row,4)=$log
+	        $row++
+        } 
+}
 
 #Resize Excel columns and save report on Desktop
-	Write-Host "`nAll builds finished, please check status in the Report" -ForegroundColor Green
+function Save-ExcelFile ()
+{
+    param
+    (
+        [object]$excel
+    )
+    Write-Host "`nAll builds finished, please check status in the Report"
 	Write-Host "`nReport was saved on your Desktop in Excel file!" 
 	$date = Get-Date -Format "MM/dd/yyyy_HH_mm_ss"
-	$output_path = "C:\Users\$env:username\Desktop\Report_$date.xlsx"
-	$usedRange = $sheet.UsedRange
-	$usedRange.EntireColumn.AutoFit() | Out-Null
-	$workbook.SaveAs($output_path)
+	$excel.ActiveSheet.UsedRange.EntireColumn.AutoFit() | Out-Null
+	$excel.ActiveWorkbook.SaveAs("C:\Users\$env:username\Desktop\Report_$date.xlsx")
 	$excel.Quit()
+}
 
 
